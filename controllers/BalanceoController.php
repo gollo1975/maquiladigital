fa921c0df52885a0de0e906c5e681218baf243cd
gollo1975 <jgpmorales1975@hotmail.com>
2022-07-23 12:26:11 -0500

    vista
    Se modifico la vista de balanceo

--- controllers/BalanceoController.php
+++ controllers/BalanceoController.php
@@ -211,7 +211,7 @@ class BalanceoController extends Controller
        if ($id_proceso_confeccion == 1){
           $flujo_operaciones = FlujoOperaciones::find()->where(['=', 'idordenproduccion', $idordenproduccion])->andWhere(['=','operacion', 0])->orderBy('pieza, operacion, orden_aleatorio asc')->all();
        }else{
-          $flujo_operaciones = FlujoOperaciones::find()->where(['=', 'idordenproduccion', $idordenproduccion])->andWhere(['=','operacion', 1])->orderBy('pieza, operacion, orden_aleatorio asc')->all(); 
+          $flujo_operaciones = FlujoOperaciones::find()->where(['=', 'idordenproduccion', $idordenproduccion])->orderBy('pieza, operacion, orden_aleatorio asc')->all(); 
        }   
         $balanceo_detalle = BalanceoDetalle::find()->where(['=', 'id_balanceo', $id])->orderBy('id_operario asc')->all();
         $operario = \app\models\Operarios::find()->where(['=','estado', 1])->orderBy('nombrecompleto ASC')->all();

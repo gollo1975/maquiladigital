3e7b6cb537740259c1f2eeea5beeea32d619b30d
gollo1975 <jgpmorales1975@hotmail.com>
2023-12-29 18:21:27 -0500

    model

---
+++ models/ModeloBuscarOperario.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+
+/**
+ * ContactForm is the model behind the contact form.
+ */
+class ModeloBuscarOperario extends Model
+{
+    
+    public $operario;
+    public $fecha_entrada;
+    public $aplica_sabado;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['operario','aplica_sabado'], 'integer'],
+            ['fecha_entrada', 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+           'operario' => 'Nombre operario:',
+           'fecha_entrada' => 'Fecha confección:',
+           'aplica_sabado' => 'Aplica sabado:', 
+            
+        ];
+    }
+     
+    
+}

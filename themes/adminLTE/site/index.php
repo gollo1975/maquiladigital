0a7a83b101754591d94353400fcc558b8c6393eb
gollo1975 <jgpmorales1975@hotmail.com>
2024-01-03 17:29:59 -0500

    index
    Se modificó la vista del index

--- themes/adminLTE/site/index.php
+++ themes/adminLTE/site/index.php
@@ -5,6 +5,8 @@ use app\models\Matriculaempresa;
 use app\models\Users;
 /* @var $this yii\web\View */
 $empresa = Matriculaempresa::findOne(1);
+$municipio = \app\models\Municipio::find()->all();
+$departamento = app\models\Departamento::find()->all();
 $operario = app\models\Operarios::find()->where(['=','estado', 1])->all();
 $ordenConfeccion = \app\models\Ordenproduccion::find()->where(['=','cerrar_orden', 0])->andWhere(['=','idtipo', 1])->all();
 $ordenTerminacion = \app\models\Ordenproduccion::find()->where(['=','cerrar_orden', 0])->andWhere(['=','idtipo', 2])->all();
@@ -12,78 +14,180 @@ $cliente = \app\models\Cliente::find()->where(['=','proceso', 1])->all();
 $this->title = $empresa->nombresistema;
 $this->params['breadcrumbs'][] = ['label' => 'Systime', 'url' => ['index']];
 ?>
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="card">
-                    <div class="jumbotron">
-                        <h1>Bienvenidos!</h1>
-                        <img src="dist/images/logos/logomaquila.png" align ='center' width="200px;">
-                   </div>
-              </div>
-            </div>
-        </div>
-    </div>
-</div>
-<section class="content">
-    <div class="container-fluid">
+<div class="panel panel-success">
+    <div class="login-logo">
+        <a href="#"><b><?= $empresa->nombresistema ?></a>
+   </div>
+    <div class="panel-body">
+        <section class="content">
+            <div class="container-fluid" style="text-align: center">
         <!-- Small boxes (Stat box) -->
-        <div class="row">
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-green-active">
-                    <div class="inner">
-                        <h3 style="text-align: center; color: #FFFFFF;">OPERARIOS</h3>
-                         <h3 style="text-align: center;"><?= count($operario)?></h3>
-                    </div>
-                    <div class="icon">
+            <div class="row">
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-light-blue">
+                        <div class="inner">
+                            <h4 style="text-align: center; color: #FFFFFF;"><span class='glyphicon glyphicon-home'> <font face="arial">CLIENTES</font></span></h4>  
+                            <h3 style="text-align: center;"><?= count($cliente)?></h3>
+                        </div>
+                        <div class="icon">
+                        </div>
                     </div>
                 </div>
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-success">
+                      <div class="inner">
+                        <h4 style="text-align: center; color: #2B5DB0;"><span class='glyphicon glyphicon-globe'> <font face="arial">DEPARTAMENTOS</font></span></h4>  
+                        <h3 style="text-align: center;"><?= count($departamento)?></h3>
+                      </div>
+                      <div class="icon">
+                      </div>
+                    </div>
+                </div>    
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-olive-active">
+                      <div class="inner">
+                        <h4 style="text-align: center; color: white"><span class='glyphicon glyphicon-user'> <font face="arial">OPERARIOS</font></span></h4>
+                        <h3 style="text-align: center;"><?= count($operario)?></h3>
+                      </div>
+                      <div class="overlay">
+                        <i class="fas fa-2x fa-sync-alt fa-spin"></i>
+                      </div>
+                    </div>
+                </div>  
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-info">
+                      <div class="inner">
+                          <h4 style="text-align: center; color: #253886;"><span class='glyphicon glyphicon-user'> <font face="arial">ORDENES DE COMPRA</font></span></h4>
+                        <h3 style="text-align: center;"><?= count($ordenConfeccion)?></h3>
+                      </div>
+                      <div class="overlay">
+                        <i ></i>
+                      </div>
+                    </div>
+                </div>  
             </div>
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-success">
-                  <div class="inner">
-                    <h3 style="text-align: center; color: #2B5DB0;">CONFECCION</h3>
-                    <h3 style="text-align: center;"><?= count($ordenConfeccion)?></h3>
-                  </div>
-                  <div class="icon">
-                   
-                  </div>
-                </div>
-            </div>    
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-danger">
-                  <div class="inner">
-                    <h3 style="text-align: center; color: #7458A7;">TERMINACION</h3>
-                    <h3 style="text-align: center;"><?= count($ordenTerminacion)?></h3>
-                  </div>
-                 <div class="overlay">
-                    <i class="fas fa-2x fa-sync-alt fa-spin"></i>
-                  </div>
-                </div>
-            </div>  
-            <div class="col-lg-3 col-6">
-                <div class="small-box bg-info">
-                  <div class="inner">
-                    <h3 style="text-align: center; color: #253886;">CLIENTES</h3>
-                    <h3 style="text-align: center;"><?= count($cliente)?></h3>
-                  </div>
-                 <div class="overlay">
-                    <i class="fas fa-2x fa-sync-alt fa-spin"></i>
-                  </div>
-                </div>
-            </div>  
-           
         </div>
+    </section> 
+    </div>  
+<div class="container my-5">
+
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: white; width: 100%; text-align: center"
+          >
+    <!-- Section: Social media -->
+    <section
+             class="d-flex justify-content-between p-4"
+             style="background-color: #136C5D; text-align: center"
+             >
+      <!-- Left -->
+      <div class="me-5">
+          <span class="badge"> <h4><font face="arial">MAQUILA DIGITAL SAS</font></h4></span>
+      </div>
+      <!-- Left -->
+
+      <!-- Right -->
+      
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h4 class="text-uppercase fw-bold"><b><span class="badge"><font face="arial">NUESTRA COMPAÑIA</font></span></b></h4>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 100px; background-color: black; height: 3px"
+                />
+            <p>
+                <b>MAQUILA DIGITAL SAS</b>, Es una empresa especializada en procesos de Maquila, diseño y confeccion de prendas de vestir.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold"><span class="badge"><font face="arial">PRODUCTOS</font></span></h6>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 100px; background-color: black; height: 3px"
+                />
+            <p>
+              <a href="#!" class="text-black">Systime </a>
+            </p>
+            <p>
+              <a href="#!" class="text-black">Diamante ERP</a>
+            </p>
+            <p>
+              <a href="#!" class="text-black">Producción</a>
+            </p>
+            <p>
+              <a href="#!" class="text-black">Inventarios</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h4 class="text-uppercase fw-bold"><span class="badge"><font face="arial">SERVICIOS</font></span></h4>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 100px; background-color: black; height: 3px"
+                />
+            <p>
+              <a href="#!" class="text-black">Maquila de confección</a>
+            </p>
+            <p>
+              <a href="#!" class="text-black">Paquete completo</a>
+            </p>
+            <p>
+              <a href="#!" class="text-black">Venta de servicios TIC</a>
+            </p>
+           
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h4 class="text-uppercase fw-bold"><span class="badge"><font face="arial">CONTACTO</font></span></h4>
+            <hr
+                class="mb-4 mt-0 d-inline-block mx-auto"
+                style="width: 100px; background-color: black; height: 3px"
+                />
+            <p><i class='glyphicon glyphicon-user'></i>  Medellin - Colombia</p>
+            <p><i class='glyphicon glyphicon-envelope'></i>  facturacio@maquiladigital.com.co</p>
+            <p><i class='glyphicon glyphicon-phone'></i>  323 308 36 29</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div
+         class="text-center p-4"
+         style="background-color: #136C5D"
+         >
+        <h4 class="text-uppercase fw-bold"><span class="badge"> <font face="arial">© 2023 Copyright | Todos los dereechos reservados | MAQUILA DIGITAL SAS . Version 2.0</font> </span></h4>
     </div>
-</section> 
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</div>
 
-<footer class="main-footer">
-    <strong>Copyright &copy; 2022 | Todos los derechos reservados <a href="https://www.tiendaupfashion.com"> | Maquila Digital sas</a>. <b>Version</b> 2.0</strong>
-</footer>
-
-
-
+</div>   
 
 
  

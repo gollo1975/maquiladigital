5739adc2913d748270034888f53fb690f9c5fac6
gollo1975 <jgpmorales1975@hotmail.com>
2025-05-07 18:34:17 -0500

    controller

--- controllers/ProgramacionNominaController.php
+++ controllers/ProgramacionNominaController.php
@@ -576,7 +576,7 @@ class ProgramacionNominaController extends Controller {
                                     CURLOPT_RETURNTRANSFER => true,
                                     CURLOPT_ENCODING => '',
                                     CURLOPT_MAXREDIRS => 10,
-                                    CURLOPT_TIMEOUT => 0,
+                                    CURLOPT_TIMEOUT => 120,
                                     CURLOPT_FOLLOWLOCATION => true,
                                     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
                                     CURLOPT_CUSTOMREQUEST => 'POST',

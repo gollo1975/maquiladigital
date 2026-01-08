4e11f0647b9e844ed138c9a160180ef8a0e29ba7
gollo1975 <jgpmorales1975@hotmail.com>
2026-01-07 22:28:43 -0500

    contrroller

--- controllers/CostoProduccionDiariaController.php
+++ controllers/CostoProduccionDiariaController.php
@@ -204,7 +204,7 @@ class CostoProduccionDiariaController extends Controller {
                         $salario = \app\models\ConfiguracionSalario::find()->where(['=','estado', 1])->one();
                         $conPension = \app\models\ConfiguracionPension::findOne(1);
                         $caja = \app\models\CajaCompensacion::findOne(1);
-                        $salud = \app\models\ConfiguracionEps::findOne(3);
+                        $salud = \app\models\ConfiguracionEps::find()->where(['=','codigo_api_nomina', 1 ])->one();
                          if(!$salud){
                                 Yii::$app->getSession()->setFlash('error', 'Error en la configuracion en el codigo del porcentaje de la EPS. Valide la tabla configuracionEPS.  ');
                                 return $this->redirect(['simuladorsalario']);
